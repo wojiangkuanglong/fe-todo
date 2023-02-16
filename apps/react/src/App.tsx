5202a3c57200acc5f7a4b731ca49194e4d75960b
ChengZhenghao <chengzhenghao@kuaishou.com>
2023-02-16 14:37:21 +0800

    feat(eslint): rm some unuse lint

--- apps/react/src/App.tsx
+++ apps/react/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  console.log(import.meta.env, process.env.TEST_TOKEN)
+  console.log(import.meta.env, process.env.TEST_TOKEN);
   return (
     <div className="App">
       <div>
